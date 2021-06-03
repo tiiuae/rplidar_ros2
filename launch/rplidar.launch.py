d259742e769ed97f2ca5b6a7b488840291358ba9
Sergey Smirnov <sergey.smirnov@unikie.com>
2021-06-03 15:47:32 +0300

    fixes to run rplidar on the drone

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -1,11 +1,11 @@
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 from os import path
-
+import os
 from launch import LaunchDescription
 
-
 def generate_launch_description():
+    DRONE_DEVICE_ID=os.getenv('DRONE_DEVICE_ID')
     return LaunchDescription([
         Node(
             node_name='rplidar',
@@ -13,8 +13,9 @@ def generate_launch_description():
             node_executable='rplidar',
             output='screen',
             parameters=[
-                path.join(get_package_share_directory('rplidar_ros'), 'config',
-                          'rplidar.yaml')
+                path.join(get_package_share_directory('rplidar_ros2'), 'config',
+                          'rplidar.yaml'),
+                {'topic_name': DRONE_DEVICE_ID + '/rplidar/scan'}
             ],
         ),
     ])
